b900e44165b7b816281843bb703d92337b52f98e
andreick <andreick.gomes.ismart@gmail.com>
2026-02-22 19:49:29 -0300

    feat(sensor): add String to TSID converter and endpoints

--- src/main/java/com/example/algasensors/device/management/api/controller/SensorController.java
+++ src/main/java/com/example/algasensors/device/management/api/controller/SensorController.java
@@ -6,13 +6,21 @@ import com.example.algasensors.device.management.common.IdGenerator;
 import com.example.algasensors.device.management.domain.model.Sensor;
 import com.example.algasensors.device.management.domain.model.SensorId;
 import com.example.algasensors.device.management.domain.repository.SensorRepository;
+import io.hypersistence.tsid.TSID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RequiredArgsConstructor
 @RestController
@@ -20,6 +28,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class SensorController {
 
     private final SensorRepository sensorRepository;
+
+    @GetMapping
+    public Page<SensorOutput> getSensors(@PageableDefault @NonNull Pageable pageable) {
+        return sensorRepository.findAll(pageable)
+                .map(SensorOutput::fromEntity);
+    }
+
+    @GetMapping("/{sensorId}")
+    public SensorOutput getSensorById(@PathVariable("sensorId") TSID sensorId) {
+        return sensorRepository.findById(new SensorId(sensorId))
+                .map(SensorOutput::fromEntity)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
